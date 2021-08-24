9d422dc56420b2843e7d3e6337a7b515e2a7c9be
leobezr <le.bezr@gmail.com>
2021-08-24 20:18:47 -0300

    core: bot

---
+++ src/bot.ts
@@ -0,0 +1,7 @@
+import RuneScape from "./games/runescape";
+import Tibia from "./games/tibia"
+
+export default class Bot {
+   public static RuneScape = RuneScape;
+   public static Tibia = Tibia;
+}
